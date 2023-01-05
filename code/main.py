38404b4a8a050cb62d92164c7196322d726c94fe
Matheus Vidal Grunewald <contato.matheusvidal@gmail.com>
2023-01-05 01:22:59 -0300

    fake commit

--- code/main.py
+++ code/main.py
@@ -15,7 +15,7 @@ def hello(name: str, idade: int, display_idade: bool = True):
 def commit(data: str):
     os.system(f"echo \'{data}\' > foo.txt")
     os.system("git add .")
-    os.system(f'git commit -m \"fake commit\" --date \"{data}\"')
+    os.system(f'git commit --quiet -m \"fake commit\" --date \"{data}\"')
     os.system("git push")
 
 if __name__ == "__main__":
