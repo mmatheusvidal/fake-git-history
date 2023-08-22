3c534202cfa4e5135f3a372f61af4d200b588f04
Matheus Vidal Grunewald <contato.matheusvidal@gmail.com>
2023-08-22 00:58:14 -0300

    teste

---
+++ code/main.py
@@ -0,0 +1,19 @@
+import typer
+import os
+
+app = typer.Typer()
+
+@app.command()
+def hello(name: str, idade: int, display_idade: bool = True):
+    print(f'É nóis {name}')
+    if display_idade:
+        print(f'{idade}')
+
+@app.command()
+def goodbye():
+    os.system("git add .")
+    os.system("git commit -m teste")
+    os.system("git push")
+
+if __name__ == "__main__":
+    app()
